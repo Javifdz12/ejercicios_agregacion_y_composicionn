88c6f1a87b8fd86bdfe06b28efb1a2efc1a3af2d
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-03-16 20:15:07 +0100

    gg

---
+++ ejercicio4.py
@@ -0,0 +1,27 @@
+class zoo:
+    def __init__(self,stock,cuidadores,animales):
+        pass
+class cuidador:
+    def __init__(self,animales,vacaciones):
+        pass
+class animal:
+    def __init__(self,dieta):
+        pass
+class vacaciones:
+    def __init__(self):
+        pass
+class comida:
+    def __init__(self,dieta):
+        pass
+class stock:
+    def __init__(self,comida):
+        pass
+class carnivoros:
+    def __init__(self):
+        pass
+class herbivoros:
+    def __init__(self):
+        pass
+class insectivoros:
+    def __init__(self):
+        pass
